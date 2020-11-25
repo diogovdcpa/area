28e2b4b26123a9e81dd0591bef047c3aa22c7919
Eng.Diogo <dev.diogovdcpa@gmail.com>
2020-11-25 14:43:48 -0300

    finalizando projeto calcula area

--- app/controllers/AreaController.php
+++ app/controllers/AreaController.php
@@ -1,11 +1,57 @@
 <?php
-namespace app\controllers;
-use app\core\Controller;
 
-class AreaController extends Controller{
+namespace app\controllers;
+
+use app\core\Controller;
+use app\models\Area;
+
+class AreaController extends Controller
+{
+
+    public function index()
+    {
+        $dados["view"] = "formas";
+        $this->load("template", $dados);
+    }
+
+    public function calcula(){
+
+        $objArea = new Area();
+        $area = null;
+        $formula = null;
+        $figura = $_POST["figura"];
+        $base=$_POST["base"];
+        $altura=$_POST["altura"];
+        $raio=$_POST["raio"];
+
+        if($figura=="quadrado"){
+            $area = $objArea->quadrado($base);
+            $formula = "base x base";
+        }elseif($figura=="circulo"){
+            $area = $objArea->circulo($raio);
+            $formula = "pi * raio * raio";
+        }elseif($figura=="retangulo"){
+            $area = $objArea->retangulo($base,$altura);
+            $formula = "base x altura";
+        }elseif($figura=="triangulo"){
+            $area = $objArea->triangulo($base,$altura);
+            $formula = "(base x altura)/2";
+        }else{
+            $area = 0;
+        }
+
+
+        $dados["figura"] = $figura;
+        $dados["base"] = $base;
+        $dados["altura"] = $altura;
+        $dados["raio"] = $raio;
+        $dados["area"] = $area;
+        $dados["formula"] = $formula;
+
     
-   public function index(){
-       $dados['view']="formas";
-       $this->load("template",$dados);
-   } 
+        $dados["view"] = "formas";
+        $this->load("template", $dados);
+
+    }
+
 }
